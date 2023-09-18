43aa7fc913e564de7b05935ed8f5abdafdae563c
AvishkaUmayanga <avishkas97@gmail.com>
2023-09-18 12:47:25 +0530

    commit

---
+++ js/sectionfour.js
@@ -0,0 +1,42 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+
+	const itemContainer = document.querySelector('.item-container');
+	const nextBtn = document.querySelector('.nxt-button');
+	const preBtn = document.querySelector('.pre-button');
+
+	let currenIndex = 0;
+
+	function showItems() {
+		const items = itemContainer.querySelectorAll('.item');
+
+		items.forEach((currentItem,index) =>{
+			if(index===currenIndex || index===currenIndex+1){
+				currentItem.style.display = 'block';
+			}
+			else{
+				currentItem.style.display = 'none';
+			}
+		})
+	}
+
+	function nextItem(){
+		console.log('Next button clicked');
+		const items = itemContainer.querySelectorAll('.item');
+		if(currenIndex+2 < items.length){
+			currenIndex+=1;
+			showItems();
+		}
+	}
+
+	function preItems(){
+		if(currenIndex>0){
+			currenIndex-=1;
+			showItems();
+		}
+	}
+
+	showItems();
+	nextBtn.addEventListener('click',nextItem);
+	preBtn.addEventListener('click',preItems);
+});
---
+++ js/sectiontwo.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const cardContainer = document.querySelector('.card-container');
+    const nextButton = document.querySelector('.next');
+    const prevButton = document.querySelector('.prev');
+
+    let currentIndex = 0;
+
+    function showCards(){
+        const cards = cardContainer.querySelectorAll('.card');
+
+        cards.forEach((currentCard,index) =>{
+            if(index === currentIndex || index === currentIndex+1 || index === currentIndex+2){
+                currentCard.style.display = 'block';
+            }
+            else{
+                currentCard.style.display = 'none';
+            }
+        });
+    }
+
+    function nextCard() {
+        const cards = cardContainer.querySelectorAll('.card');
+
+        if(currentIndex+3 < cards.length){
+            currentIndex+=1;
+            showCards();
+        }
+    }
+
+    function prevCard(){
+        if(currentIndex>0){
+            currentIndex-= 1;
+            showCards();
+        }
+    }
+
+    showCards();
+    nextButton.addEventListener('click',nextCard);
+    prevButton.addEventListener('click',prevCard);
+})
